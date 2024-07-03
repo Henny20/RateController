8f9b230eae86e4eef1a8ef6314f8b4722af9d37c
Henny20 <47795195+Henny20@users.noreply.github.com>
2024-07-03 14:15:45 +0200

    Add files via upload

--- Models/Relay.cs
+++ Models/Relay.cs
@@ -1,1 +1,18 @@
+ namespace RateController.Models;
+ 
+ public class Relay
+    {
+        public int Id { get; set; }
 
+        public string Type { get; set; }
+
+        public int SectionNumber { get; set; }
+        /*********
+         public Relay(string id , string type, int sectionNumber)
+          {
+		    Id = id;
+		    Type = type;
+		    SectionNumber = sectionNumber;
+		}
+		**************/
+}		
